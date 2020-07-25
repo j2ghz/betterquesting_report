f357c467f279c7f13f317637dff43cf5b73e5572
J2ghz <j2.00ghz@gmail.com>
2020-07-25 14:48:43 +0200

    reorganize

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 use ::betterquesting_report::parsers;
+use betterquesting_report::{self, strip_formatting};
 use chrono::TimeZone;
 use chrono::{DateTime, Local, Utc};
 use rocket::State;
@@ -58,73 +59,12 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
-fn strip_formatting(str: &str) -> String {
-    let mut result = String::new();
-    let mut chars = str.chars();
-    while let Some(c) = chars.next() {
-        if c == '§' {
-            chars.next().unwrap();
-        } else {
-            result.push(c);
-        }
-    }
-    result
-}
-
-fn clone_and_push<T: Clone>(v: &[T], i: T) -> Vec<T> {
-    let mut v = v.to_vec();
-    v.push(i);
-    v
-}
-
 #[get("/quests/history")]
 fn quests_history(file_loc: State<FileLocation>) -> Template {
-    let db =
-        parsers::quest_database::parse(file_loc.betterquesting.join("QuestDatabase.json")).unwrap();
-    let quests = db
-        .quest_database
-        .iter()
-        .map(|(_id, q)| (q.quest_id, q.clone()))
-        .collect::<HashMap<i64, parsers::quest_database::Quest>>();
-    let users = parsers::name_cache::parse(file_loc.betterquesting.join("NameCache.json"))
-        .unwrap()
-        .name_cache
-        .iter()
-        .map(|(_id, i)| (i.uuid.clone(), i.name.clone()))
-        .collect::<HashMap<String, String>>();
-    let completions =
-        crate::parsers::quest_progress::parse(file_loc.betterquesting.join("QuestProgress.json"))
-            .unwrap();
+    let data = parsers::load_data(&file_loc.betterquesting);
 
-    let quest_ql: HashMap<i64, String> = db
-        .quest_lines
-        .iter()
-        .flat_map(|(_id, ql)| {
-            let name = ql.properties.betterquesting.name.clone();
-            ql.quests
-                .iter()
-                .map(|(_id, q)| (q.id.clone(), name.clone()))
-                .collect::<Vec<_>>()
-                .into_iter()
-        })
-        .collect();
-
-    let mut quest_unlocks: HashMap<i64, Vec<i64>> = HashMap::new();
-
-    for (_id, q) in &quests {
-        for prereq_id in &q.pre_requisites {
-            if quest_unlocks.contains_key(&prereq_id) {
-                let old = quest_unlocks.get(&prereq_id).unwrap().to_vec();
-                quest_unlocks
-                    .insert(prereq_id.clone(), clone_and_push(&old, q.quest_id))
-                    .unwrap();
-            } else {
-                quest_unlocks.insert(prereq_id.clone(), vec![q.quest_id]);
-            }
-        }
-    }
-
-    let mut items = completions
+    let mut items = data
+        .completions
         .quest_progress
         .iter()
         .flat_map(|(_id, q)| {
@@ -151,11 +91,12 @@ fn quests_history(file_loc: State<FileLocation>) -> Template {
             last_date = item.timestamp.with_timezone(&Local).date();
             day = Vec::new();
         }
-        let quest = quests.get(&item.id).unwrap();
+        let quest = data.quests.get(&item.id).unwrap();
         day.push(QuestDetails {
             id: item.id,
             name: strip_formatting(&quest.properties.betterquesting.name),
-            questline: quest_ql
+            questline: data
+                .quest_ql
                 .get(&item.id)
                 .unwrap_or(&"Questline not found".to_string())
                 .clone(),
@@ -166,22 +107,24 @@ fn quests_history(file_loc: State<FileLocation>) -> Template {
                 .lines()
                 .map(|s| s.to_string())
                 .collect(),
-            user: users.get(&item.user).unwrap().clone(),
+            user: data.users.get(&item.user).unwrap().clone(),
             time: item
                 .timestamp
                 .with_timezone(&Local)
                 .format("%T")
                 .to_string(),
-            unlocks: quest_unlocks
+            unlocks: data
+                .quest_unlocks
                 .get(&item.id)
                 .iter()
                 .flat_map(|x| x.iter())
                 .map(|ref_quest_id| {
-                    let quest = quests.get(&ref_quest_id).unwrap();
+                    let quest = data.quests.get(&ref_quest_id).unwrap();
                     QuestRef {
                         id: ref_quest_id.clone(),
                         name: strip_formatting(&quest.properties.betterquesting.name),
-                        questline: quest_ql
+                        questline: data
+                            .quest_ql
                             .get(ref_quest_id)
                             .unwrap_or(&"Questline not found".to_string())
                             .clone(),
--- src/parsers/mod.rs
+++ src/parsers/mod.rs
@@ -1,10 +1,89 @@
+use chrono::{DateTime, TimeZone, Utc};
+use quest_database::Quest;
+use serde::Serialize;
+use std::{collections::HashMap, path::Path};
+
 pub mod name_cache;
 pub mod quest_database;
 pub mod quest_progress;
 pub mod questing_parties;
 
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Serialize)]
+pub struct QuestCompletion {
+    timestamp: DateTime<Utc>,
+    user: String,
+    id: i64,
+}
+
+#[derive(Debug, Serialize)]
+pub struct Data {
+    pub quests: HashMap<i64, Quest>,
+    pub users: HashMap<String, String>,
+    pub quest_ql: HashMap<i64, String>,
+    pub quest_unlocks: HashMap<i64, Vec<i64>>,
+    pub completions: quest_progress::Root,
+}
+
+pub fn load_data<P: AsRef<Path>>(dir: P) -> Data {
+    let db = quest_database::parse(dir.as_ref().join("QuestDatabase.json")).unwrap();
+    let quests = db
+        .quest_database
+        .iter()
+        .map(|(_id, q)| (q.quest_id, q.clone()))
+        .collect::<HashMap<i64, quest_database::Quest>>();
+    let users = name_cache::parse(dir.as_ref().join("NameCache.json"))
+        .unwrap()
+        .name_cache
+        .iter()
+        .map(|(_id, i)| (i.uuid.clone(), i.name.clone()))
+        .collect::<HashMap<String, String>>();
+
+    let quest_ql: HashMap<i64, String> = db
+        .quest_lines
+        .iter()
+        .flat_map(|(_id, ql)| {
+            let name = ql.properties.betterquesting.name.clone();
+            ql.quests
+                .iter()
+                .map(|(_id, q)| (q.id.clone(), name.clone()))
+                .collect::<Vec<_>>()
+                .into_iter()
+        })
+        .collect();
+
+    let mut quest_unlocks: HashMap<i64, Vec<i64>> = HashMap::new();
+
+    for (_id, q) in &quests {
+        for prereq_id in &q.pre_requisites {
+            if quest_unlocks.contains_key(&prereq_id) {
+                let old = quest_unlocks.get(&prereq_id).unwrap().to_vec();
+                quest_unlocks
+                    .insert(prereq_id.clone(), {
+                        let mut new: Vec<_> = old.as_slice().to_vec();
+                        new.push(q.quest_id);
+                        new
+                    })
+                    .unwrap();
+            } else {
+                quest_unlocks.insert(prereq_id.clone(), vec![q.quest_id]);
+            }
+        }
+    }
+
+    let completions = quest_progress::parse(dir.as_ref().join("QuestProgress.json")).unwrap();
+
+    Data {
+        quests,
+        users,
+        quest_ql,
+        quest_unlocks,
+        completions,
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::load_data;
     use chrono::prelude::*;
     use std::collections::{BinaryHeap, HashMap};
 
@@ -13,6 +92,11 @@ mod tests {
         timestamp: DateTime<Utc>,
         user: String,
         id: i64,
+    }
+
+    #[test]
+    fn desrialize_sample_all() {
+        load_data("./sample/1");
     }
 
     #[test]
